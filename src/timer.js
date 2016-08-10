566daa6ebcd693eb24e1d4ee690472a9c618804d
nigelgilbert <ngilbert@smu.edu>
2016-08-09 19:38:07 -0500

    ready to hack on bruh bruh

--- src/timer.js
+++ src/timer.js
@@ -5,9 +5,8 @@ var isRunning = false;
 var interval = null;
 var mode = ""; // "pomodoro" || "short-break" || "long-break"
 
-writeTimeToHtml();
-
 module.exports.start = function() {
+  console.log('start()');
   // - check if remainingTime is not null.
   // - if it is null, end function. else, execute the rest of the function.
   // - set isRunning to true
@@ -16,37 +15,42 @@ module.exports.start = function() {
 };
 
 module.exports.stop = function() {
+  console.log('stop()');
   // - stop the interval
   // - set isRunning to false
 };
 
 module.exports.reset = function() {
+  console.log('reset()');
   // - if mode is "pomodoro", call pomodoro function to reset the time.
   // ...do the same check for the other 2 modes.
 };
 
 module.exports.pomodoro = function() {
+  console.log('pomodoro()');
   // - stop timer
   // - set endTime to 25 minutes in the future
   // - hint: http://preview.tinyurl.com/ncnn8le
 };
 
 module.exports.shortBreak = function() {
+  console.log('shortBreak()');
   // - stop timer
   // - set endTime for 5 minutes in the future
 };
 
 module.exports.longBreak = function() {
+  console.log('longBreak()');
   // - stop timer
   // - set endTime for 15 minutes in the future
 };
 
-function writeTimeToHtml(timeobj) {
-  var timestring; 
-  if (timeobj === "undefined") {
+module.exports.writeTimeToHtml = function(timeobj) {
+  var timestring;
+  if (typeof timeobj === "undefined") {
     timestring = "0:00";
   } else {
-    timestring = timeobj.minutes + ":" + timeob.seconds;
+    timestring = timeobj.minutes + ":" + timeobj.seconds;
   }
   document.getElementById('timer').innerHTML = timestring;
 }
